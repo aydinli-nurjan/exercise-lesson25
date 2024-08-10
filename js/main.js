badebcf1e797f792bb1841854bad1db7fdd426bd
Khadija Aydinli <khadijaaydinligmail.com@Khadijas-MacBook-Air.local>
2024-08-10 13:39:05 +0400

    up button

--- js/main.js
+++ js/main.js
@@ -19,3 +19,26 @@ tl.from(".mm", {opacity: 0, y:30, })
 tl.from(".n", {opacity: 0, y:30, })
 tl.add(btnAnimation);
 
+
+const goTopBtn = document.querySelector(".go-top");
+
+window.addEventListener("scroll", trackScroll);
+goTopBtn.addEventListener("click", goTop);
+
+function trackScroll() {
+  const scrolled = window.pageYOffset;
+  const coords = document.documentElement.clientHeight;
+
+  if (scrolled > coords) {
+    goTopBtn.classList.add("go-top--show");
+  } else {
+    goTopBtn.classList.remove("go-top--show");
+  }
+}
+
+function goTop() {
+  if (window.pageYOffset > 0) {
+    window.scrollBy(0, -136);
+    setTimeout(goTop, 0);x
+  }
+}
